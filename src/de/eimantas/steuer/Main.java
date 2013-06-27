e0a741bfbcf30f9529adc3db67ccf24e6e67984d
Eimantas <Eimantas@Eimantas-PC.Speedport_W_723V_1_28_000>
2013-06-27 21:09:59 +0200

    GWT Support added

--- src/de/eimantas/steuer/Main.java
+++
@@ -1,9 +0,0 @@
-package de.eimantas.steuer;
-
-public class Main {
-
-	public static void main(String[] args) {
-		System.out.println("tests");
-	}
-
-}
---
+++ src/de/eimantas/steuer/server/Tester.java
@@ -0,0 +1,19 @@
+package de.eimantas.steuer.server;
+
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+
+public class Tester {
+
+	public static void main(String[] args) throws IOException,
+			InvalidKeyException, IllegalBlockSizeException,
+			BadPaddingException, NoSuchAlgorithmException,
+			NoSuchPaddingException {
+
+	}
+}
