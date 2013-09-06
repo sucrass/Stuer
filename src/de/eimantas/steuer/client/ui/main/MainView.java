e8b2ee2496072c78b0f62d92e0aaea1c926bdf15
Eimantas <Eimantas@Eimantas-PC>
2013-09-06 23:17:03 +0200

    test

--- src/de/eimantas/steuer/client/ui/main/MainView.java
+++ src/de/eimantas/steuer/client/ui/main/MainView.java
@@ -1,9 +1,13 @@
 package de.eimantas.steuer.client.ui.main;
 
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.DockPanel;
 
 public class MainView extends Composite {
 	public MainView() {
+
+		DockPanel dockPanel = new DockPanel();
+		initWidget(dockPanel);
 	}
 
 }
