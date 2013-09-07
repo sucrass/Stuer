10fe7e318ff25e3ea6ef6b7bd9ad5a34ac9a685d
Eimantas <Eimantas@Eimantas-PC>
2013-09-07 02:24:40 +0200

    test

--- src/de/eimantas/steuer/client/ui/main/UIButton.java
+++ src/de/eimantas/steuer/client/ui/main/UIButton.java
@@ -1,7 +1,10 @@
 package de.eimantas.steuer.client.ui.main;
 
 import com.google.gwt.user.client.ui.Composite;
+import com.sencha.gxt.widget.core.client.button.TextButton;
 
 public class UIButton extends Composite {
+
+	TextButton button = new TextButton();
 
 }
