c69dddfe5efe671f426045993f87e2b4302b5c37
Eimantas <Eimantas@Eimantas-PC>
2013-09-07 03:22:41 +0200

    test

---
+++ src/de/eimantas/steuer/client/ui/main/Images.java
@@ -0,0 +1,23 @@
+package de.eimantas.steuer.client.ui.main;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.resources.client.ClientBundle;
+import com.google.gwt.resources.client.ImageResource;
+
+public interface Images extends ClientBundle {
+
+	public Images INSTANCE = GWT.create(Images.class);
+
+	@Source("offene.png")
+	ImageResource offene();
+
+	@Source("einstellung.png")
+	ImageResource einstellung();
+
+	@Source("erfassen.png")
+	ImageResource erfassen();
+
+	@Source("home.png")
+	ImageResource home();
+
+}
